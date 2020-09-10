41de57dd8bee5c14e7af8da50a242ff2b747edbd
sergeizavialov <bd.sora@gmail.com>
2020-09-10 16:08:53 +0500

    fix with main activity_main.xml

--- app/src/main/java/ru/sergeyzavyalov/testapplication/MainActivity.kt
+++ app/src/main/java/ru/sergeyzavyalov/testapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.sergeyzavyalov.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.sergeyzavyalov.testapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
